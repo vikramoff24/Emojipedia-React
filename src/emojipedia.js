4b7926af8cb0dda4552549ee8dbcbad0ec82b083
Vikram S <60956383+vikramoff24@users.noreply.github.com>
2020-09-01 00:42:06 +0530

    Final Commit

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -26,6 +26,20 @@ const emojipedia = [
     name: "Smiling Face With Heart-Eyes",
     meaning:
       "Happily beaming face with heart-shaped eyes. Insanely in love: with a person, a place or an object. Expresses the extent of love and affection as well as gratitude and is often part of romantic messages. Just does not know what to do with all the luck."
+  },
+  {
+    id: 5,
+    emoji: "😛",
+    name: "Face With Stuck-Out Tongue",
+    meaning:
+      "Horseplay with friends, weakening an ironic remark and making sure that the other does not take seriously what has been said. Making fun of others: I knew you'd screw it up."
+  },
+  {
+    id: 6,
+    emoji: "🥳",
+    name: "Face with party blower and party hat",
+    meaning:
+      "That was a wild party or “Let's celebrate“. No matter what joyful event is in view, it will be celebrated appropriately. May also stand for the carnival season."
   }
 ];
 
